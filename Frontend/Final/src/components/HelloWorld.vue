aad5360049bba4ac4783f647302daab9c433f034
bbqms <bbqms@hotmail.com>
2019-07-06 01:57:12 -0500

    Frontend2

--- Frontend/Final/src/components/HelloWorld.vue
+++ Frontend/Final/src/components/HelloWorld.vue
@@ -1,147 +1,134 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
+  <v-layout row wrap>
+    <v-flex xs6>
+      <v-text-field
+        append-icon="search"
+        label="Filter"
+        single-line
+        hide-details
+        @input="filterSearch"
+      ></v-text-field>
+    </v-flex>
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
+    <v-flex xs6>
+      <v-select :items="authors" label="Author" @change="filterAuthor"></v-select>
+    </v-flex>
 
-      <v-flex
-        mb-5
-        xs12
+    <v-flex xs12>
+      <v-data-table
+        :headers="headers"
+        :items="rows"
+        item-key="name"
+        :search="filters"
+        :custom-filter="customFilter"
       >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <template slot="headers" slot-scope="props">
+          <tr>
+            <th v-for="header in props.headers" :key="header.text">{{ header.text }}</th>
+          </tr>
+        </template>
 
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+        <template slot="items" slot-scope="props">
+          <tr>
+            <td>{{ props.item.name }}</td>
+            <td>{{ props.item.added_by }}</td>
+          </tr>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
+export default {
+  data: () => ({
+    filters: {
+      search: "",
+      added_by: ""
+    },
 
-      ]
-    })
+    authors: ["Admin", "Editor"],
+    headers: [
+      {
+        text: "Names",
+        align: "left",
+        value: "name",
+        sortable: false
+      },
+      {
+        text: "Item addad by",
+        value: "added_by",
+        align: "left",
+        sortable: false
+      }
+    ],
+    rows: [
+      {
+        name: "Marcelo Tosco",
+        added_by: "admin"
+      },
+      {
+        name: "Carlos Campos",
+        added_by: "admin"
+      },
+      {
+        name: "Luis Gonzalez",
+        added_by: "Editor"
+      },
+      {
+        name: "Keopx",
+        added_by: "Editor"
+      },
+      {
+        name: "Marco Marocchi",
+        added_by: "Admin"
+      }
+    ]
+  }),
+
+  methods: {
+    customFilter(items, filters, filter, headers) {
+      // Init the filter class.
+      const cf = new this.$MultiFilters(items, filters, filter, headers);
+
+      cf.registerFilter("search", function(searchWord, items) {
+        if (searchWord.trim() === "") return items;
+
+        return items.filter(item => {
+          return item.name.toLowerCase().includes(searchWord.toLowerCase());
+        }, searchWord);
+      });
+
+      cf.registerFilter("added_by", function(added_by, items) {
+        if (added_by.trim() === "") return items;
+
+        return items.filter(item => {
+          return item.added_by.toLowerCase() === added_by.toLowerCase();
+        }, added_by);
+      });
+
+      // Its time to run all created filters.
+      // Will be executed in the order thay were defined.
+      return cf.runFilters();
+    },
+
+    /**
+     * Handler when user input something at the "Filter" text field.
+     */
+    filterSearch(val) {
+      this.filters = this.$MultiFilters.updateFilters(this.filters, {
+        search: val
+      });
+    },
+
+    /**
+     * Handler when user  select some author at the "Author" select.
+     */
+    filterAuthor(val) {
+      this.filters = this.$MultiFilters.updateFilters(this.filters, {
+        added_by: val
+      });
+    }
   }
+};
 </script>
-
-<style>
-
-</style>
--- Frontend/Final/src/main.js
+++ Frontend/Final/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import MultiFiltersPlugin from './plugins/MultiFilters' // <-- THIS
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(MultiFiltersPlugin);
+
 axios.defaults.baseURL='http://localhost:5050/'
 new Vue({
   router,
